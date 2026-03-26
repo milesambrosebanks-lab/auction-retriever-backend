e3cf4385c727ea7fe8708211f34a96732e3e8aeb
Sakhawat Hossain <sajuislam266@gmail.com>
2026-03-26 10:01:41 +0600

    remove path issue3

--- app/Http/Controllers/Web/Backend/AuctionListingController.php
+++ app/Http/Controllers/Web/Backend/AuctionListingController.php
@@ -124,13 +124,14 @@ class AuctionListingController extends Controller
             } elseif ($source === 'auction_com') {
                 // Run the Auction.com scrape command
                 // Artisan::call('scrape:auction', ['--limit' => 5, '--max' => 10]);
-                $output = [];
-                $status = null;
+                // $output = [];
+                // $status = null;
 
-                exec('export PATH=/home/thewarriors/.nvm/versions/node/v24.13.0/bin:/usr/local/bin:/usr/bin:/bin && php artisan scrape:auction --limit=5 --max=10 2>&1', $output, $status);
+                $path = '/home/thewarriors/milesbanks.thewarriors.team';
 
-                Log::info($output);
-                Log::info($status);
+                exec("cd $path && php artisan scrape:auction --limit=5 --max=10 > /dev/null 2>&1 &");
+                // Log::info($output);
+                // Log::info($status);
 
                 return response()->json([
                     'success' => true,
