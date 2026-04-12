c07cb852f1bc8dd877ba69575b40f7b419192e70
Sakhawat Hossain <sajuislam266@gmail.com>
2026-04-12 10:21:04 +0600

    make scraper edit

--- app/Http/Controllers/Web/Backend/AuctionListingController.php
+++ app/Http/Controllers/Web/Backend/AuctionListingController.php
@@ -140,10 +140,11 @@ class AuctionListingController extends Controller
                     Artisan::call('scrape:auction', ['--limit' => 50, '--max' => 500]);
                 } else {
                     // exec('export PATH=/home/thewarriors/.nvm/versions/node/v24.13.0/bin:/usr/local/bin:/usr/bin:/bin && cd /home/thewarriors/milesbanks.thewarriors.team && php artisan scrape:auction --limit=50 --max=500 > /dev/null 2>&1 &');
+                    Artisan::call('scrape:auction', ['--limit' => 50, '--max' => 500]);
 
-                    $path = base_path();
+                    // $path = base_path();
 
-                    exec("cd {$path} && php artisan scrape:auction --limit=50 --max=500 > storage/logs/scrape.log 2>&1 &");
+                    // exec("cd {$path} && php artisan scrape:auction --limit=50 --max=500 > storage/logs/scrape.log 2>&1 &");
                 }
 
                 return response()->json([
